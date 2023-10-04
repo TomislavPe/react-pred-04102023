9dbc63498941d85db239d1ea0e01270d27d94d6d
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-10-04 19:03:37 +0200

    vježba redux saga

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,18 +1,40 @@
-import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './features/counter/counterSlice'
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    decrement,
+    increment,
+    incrementByAmount,
+} from "./features/counter/counterSlice";
+import { getPostsFetch } from "./features/posts/postsSlice";
 
 function App() {
-  const count = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
-  return (
-    <>
-      <p>Trenutacna vrijednost je {count}</p>
-      <button onClick={() => dispatch(increment())}>Inc</button>
-      <button onClick={() => dispatch(incrementByAmount(10))}>Za iznos</button>
-      <button onClick={() => dispatch(decrement())}>Dec</button>
-    </>
-  )
+    const count = useSelector((state) => state.counter.value);
+    const { postsList, isLoading } = useSelector(
+        (state) => state.posts.postsList
+    );
+    const dispatch = useDispatch();
+    return (
+        <>
+            <div>
+                <p>Trenutacna vrijednost je {count}</p>
+                <button onClick={() => dispatch(getPostsFetch())}>Inc</button>
+                <button onClick={() => dispatch(incrementByAmount(10))}>
+                    Za iznos
+                </button>
+                <button onClick={() => dispatch(decrement())}>Dec</button>
+            </div>
+            <div>
+                <button onClick={console.log("clicked")}>Get all posts</button>
+                {!isLoading ? (
+                    postsList.map((post) => {
+                        return <div key={post.id}>{post.title}</div>;
+                    })
+                ) : (
+                    <h2>Loading...</h2>
+                )}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counter/counterSlice'
+import postsReducer from "./features/posts/postsSlice"
+import postSaga from './features/posts/postsSaga'
+import createSagaMiddleware from "redux-saga"
+
+const saga = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
-  }
+    counter: counterReducer,
+    posts: postsReducer
+  },
+  middleware: [saga]
 })
+
+saga.run(postSaga)
+
