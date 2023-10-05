67a0096e36f5172be4500bf63ab3c56959630cd0
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-10-05 16:16:36 +0200

    radi

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -10,21 +10,22 @@ import { getPostsFetch } from "./features/posts/postsSlice";
 function App() {
     const count = useSelector((state) => state.counter.value);
     const { postsList, isLoading } = useSelector(
-        (state) => state.posts.postsList
+        (state) => state.posts
     );
     const dispatch = useDispatch();
+
     return (
         <>
             <div>
                 <p>Trenutacna vrijednost je {count}</p>
-                <button onClick={() => dispatch(getPostsFetch())}>Inc</button>
+                <button onClick={() => dispatch(increment())}>Inc</button>
                 <button onClick={() => dispatch(incrementByAmount(10))}>
                     Za iznos
                 </button>
                 <button onClick={() => dispatch(decrement())}>Dec</button>
             </div>
             <div>
-                <button onClick={console.log("clicked")}>Get all posts</button>
+                <button onClick={() => dispatch(getPostsFetch())}>Get all posts</button>
                 {!isLoading ? (
                     postsList.map((post) => {
                         return <div key={post.id}>{post.title}</div>;
